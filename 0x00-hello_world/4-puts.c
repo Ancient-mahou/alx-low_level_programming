85b6dd4a0a183d497f4bf99be81588351a1b101e
Ancient-mahou <naeturx@gmail.com>
2023-02-17 01:07:44 +0100

    Task 4 commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#incldue <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ * 
+ * Return: Always 0 (Success)
+ */
+
+int main(void){
+
+	puts("\"Programing is like buidling a multilingual puzzle");
+	return (0);
+}
