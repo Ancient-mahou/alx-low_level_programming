e049bc8b08d103c120a7f11de055f573a7daefbe
Ancient-mahou <naeturx@gmail.com>
2023-02-17 01:30:49 +0100

    hotfix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,10 +6,10 @@
  * Description: A C program that prints wiht printf function.
  *
  * Return: always 0 (Success)
-*/
+ */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
