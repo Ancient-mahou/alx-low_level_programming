cc288f447209ba56792eabbd89e78823a521d10f
Ancient-mahou <naeturx@gmail.com>
2023-02-17 01:37:47 +0100

    hotfix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -7,7 +6,6 @@
  *
  * Return: always 0 (Success)
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
