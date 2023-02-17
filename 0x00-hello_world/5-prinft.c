e1be1682097f6664883438683225c4ce4add4652
Ancient-mahou <naeturx@gmail.com>
2023-02-17 01:28:05 +0100

    Task 5 commit

---
+++ 0x00-hello_world/5-prinft.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * man - Entry point
+ *
+ * Description: A C program that prints wiht printf function.
+ *
+ * Return: always 0 (Success)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is piece of art,\n");
+	return (0);
+}
