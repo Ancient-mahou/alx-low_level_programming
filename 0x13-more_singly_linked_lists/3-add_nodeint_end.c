209369659b5c909457481d82e270d14371729996
Omar Ben El Fkih <122810280+Ancient-mahou@users.noreply.github.com>
2023-04-05 22:58:40 +0000

    Update 3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -3,7 +3,7 @@
 /**
  * add_nodeint_end - add a new node at the end
  * of a linked list
- * @head: head of a list.
+ * @head: headsssss of a list.
  * @n: n element.
  *
  * Return: address of the new element. NUll if it failed.
